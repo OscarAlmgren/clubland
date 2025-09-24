9fddef1cb4d51092f7f69e5eb220ebfe12240502
Oscar Almgren <oscar.almgren@gmail.com>
2025-09-24 07:04:04 +0200

    fix: remove integration test binding from auth_flow_test.dart
    - Remove IntegrationTestWidgetsFlutterBinding.ensureInitialized() that was causing binding conflicts
    - Remove integration_test package import as this is actually a unit test
    - All auth tests now pass (30 tests total)

--- test/integration/auth_flow_test.dart
+++ test/integration/auth_flow_test.dart
@@ -2,14 +2,12 @@ import 'package:clubland/core/errors/failures.dart';
 import 'package:clubland/features/auth/domain/entities/user_entity.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../helpers/mock_providers.dart';
 import '../helpers/test_helpers.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   late MockLoginUsecase mockLoginUsecase;
 
